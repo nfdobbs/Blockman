0816a535541fc81aa1d1912fa00369b0b869100d
nfdobbs <nathanael.dobbs@jacks.sdstate.edu>
2022-07-16 05:06:27 -0600

    Changed stuff

--- src/main/java/net/dobbs/dobbs_first_mod/TutorialMod.java
+++ src/main/java/net/dobbs/dobbs_first_mod/TutorialMod.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
+	//test comment
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 	@Override
 	public void onInitialize() {
